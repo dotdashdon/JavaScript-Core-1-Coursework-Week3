1ba5fdbebc0447a61005a56757d58c280139a211
dotdashdon <80584408+dotdashdon@users.noreply.github.com>
2021-09-07 10:06:22 +0100

    Completed Ex.E
    This was my favourite so far.

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -1,4 +1,37 @@
 // Using the .map() method, create a new array with `numbers` multiplied by 100
 // Write multiple solutions using different syntax (as shown in the README)
 
+// function byHundred (number) {
+//     return number * 100;
+// }
+
+// var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+// var numbersByHundred = numbers.map(byHundred);
+
+//console.log(numbersByHundred);
+
+
+// var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+// var numbersByHundred = numbers.map(function byHundred(number) {
+//      return number * 100;
+// });
+// console.log(numbersByHundred);
+
+
+
+// var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+// var numbersByHundred = numbers.map(function (number) {
+//      return number * 100;
+// });
+// console.log(numbersByHundred);
+
+
+// var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+// var numbersByHundred = numbers.map(number => {
+//      return number * 100;
+// });
+// console.log(numbersByHundred);
+
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+var numbersByHundred = numbers.map(number =>  number * 100);
+console.log(numbersByHundred);

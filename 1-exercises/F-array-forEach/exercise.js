320efdb07bf64ea62e012c6c42cd81eee8d3fd53
dotdashdon <80584408+dotdashdon@users.noreply.github.com>
2021-09-07 12:41:48 +0100

    1st Attempt Ex. F
    This is outputting Buzz and Fizz but not FizzBuzz....also printing a list of undefined too.

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,26 @@
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+function formatNumber (number) {
+  if (number % 3 === 0) {
+    console.log ('Fizz')
+  } else if (number % 5 === 0){
+    console.log('Buzz')
+  } else if (number % 5 === 0 && number % 3 === 0) {
+    console.log('FizzBuzz')
+  } else {
+     console.log(number)
+  }
+} 
+
+arr.map(formatNumber).forEach(function(arr) {
+  console.log(arr);
+});
+
+
+console.log(arr);
+
+
 /* EXPECTED OUTPUT */
 
 /*

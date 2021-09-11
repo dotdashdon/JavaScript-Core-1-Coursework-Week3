b427284c97e0fdb91c7078e6a93829976595232b
dotdashdon <80584408+dotdashdon@users.noreply.github.com>
2021-09-11 19:28:53 +0100

    Completed Ex. J
    Hard to get my head around which char # to cite.

--- 1-exercises/J-string-substring/exercise.js
+++ 1-exercises/J-string-substring/exercise.js
@@ -6,9 +6,10 @@
 
 let statement = "I like programming and dogs";
 
-statement = statement.substring();
+statement = statement.substring(0, 18, statement.length );
 
 console.log(statement);
+
 
 /* EXPECTED OUTPUT 
 
